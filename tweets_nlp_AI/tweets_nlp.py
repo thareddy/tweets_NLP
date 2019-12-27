8798923bd1d72d8ab85327eede83a60e74cc9c09
thareddy <59251764+thareddy@users.noreply.github.com>
2019-12-27 08:40:27 +0530

    tweets in different social media by NLP

---
+++ tweets_nlp_AI/tweets_nlp.py
@@ -0,0 +1,491 @@
+﻿import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.patches as mpatches
+
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA, TruncatedSVD
+from sklearn.metrics import classification_report,confusion_matrix
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Embedding,LSTM,Dense,SpatialDropout1D, GRU
+from keras.initializers import Constant
+from keras.optimizers import Adam
+
+from nltk.corpus import stopwords
+from nltk.util import ngrams
+from nltk.tokenize import word_tokenize
+
+import tensorflow as tf
+import time 
+from IPython.core.display import display, HTML
+import plotly.graph_objects as go
+
+import os
+import re
+import gensim
+import string
+
+from graphviz import Digraph, Graph, escape, nohtml
+from wordcloud import WordCloud
+from collections import defaultdict, Counter
+from tqdm import tqdm
+
+
+
+
+
+
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+submission = (pd.read_csv("sample_submission.csv"))
+
+
+print(train.head(4))
+print(test.head(4))
+print('There are {} rows and {} columns in train'.format(train.shape[0],train.shape[1]))
+print('There are {} rows and {} columns in test'.format(test.shape[0],test.shape[1]))
+
+
+##Class distribution
+x=train.target.value_counts()
+sns.barplot(x.index,x)
+plt.gca().set_ylabel('samples')
+plt.show()
+
+##Exploratory Data Analysis of tweets
+##Number of character in a tweet
+fig,(ax1,ax2)=plt.subplots(1,2,figsize=(10,5))
+train_len=train[train['target']==1]['text'].str.len()
+ax1.hist(train_len,color='red')
+ax1.set_title('disaster train')
+train_len=train[train['target']==0]['text'].str.len()
+ax2.hist(train_len,color='green')
+ax2.set_title('Not disaster train')
+fig.suptitle('Characters in train')
+plt.show()
+
+
+##Number of words in a tweet
+fig,(ax1,ax2)=plt.subplots(1,2,figsize=(10,5))
+train_len=train[train['target']==1]['text'].str.split().map(lambda x: len(x))
+ax1.hist(train_len,color='red')
+ax1.set_title('disaster train')
+train_len=train[train['target']==0]['text'].str.split().map(lambda x: len(x))
+ax2.hist(train_len,color='green')
+ax2.set_title('Not disaster train')
+fig.suptitle('Words in a train')
+plt.show()
+
+##Average word length in a tweet
+fig,(ax1,ax2)=plt.subplots(1,2,figsize=(10,5))
+word=train[train['target']==1]['text'].str.split().apply(lambda x : [len(i) for i in x])
+sns.distplot(word.map(lambda x: np.mean(x)),ax=ax1,color='red')
+ax1.set_title('disaster')
+word=train[train['target']==0]['text'].str.split().apply(lambda x : [len(i) for i in x])
+sns.distplot(word.map(lambda x: np.mean(x)),ax=ax2,color='green')
+ax2.set_title('Not disaster')
+fig.suptitle('Average word length in each train')
+plt.show()
+
+train['length'] = train['text'].apply(len)
+data = [ go.Box(y = train[train['target']==0]['length'], name='Fake'), go.Box(y = train[train['target']==1]['length'], name='Real')]
+layout = go.Layout(title = 'Comparison of text length in Tweets ')
+plt = go.Figure(data=data, layout=layout)
+plt.show()
+print('The max len of text',len(train.text.max()))
+print('The min len of text is',len(train.text.min()))
+
+"""-----"""
+from plotly.offline import init_notebook_mode, iplot
+STOPWORDS.add('https')  # remove htps to the world Cloud
+def Plot_world(text):    
+    comment_words = ' '
+    stopwords = set(STOPWORDS) 
+    
+    for val in text: 
+        # typecaste each val to string 
+        val = str(val) 
+        # split the value 
+        tokens = val.split() 
+        # Converts each token into lowercase 
+        for i in range(len(tokens)): 
+            tokens[i] = tokens[i].lower() 
+
+        for words in tokens: 
+            comment_words = comment_words + words + ' '
+
+
+    wordcloud = WordCloud(width = 5000, height = 4000, background_color ='black', stopwords = stopwords, min_font_size = 10).generate(comment_words) 
+
+    # plot the WordCloud image                        
+    plt.figure(figsize = (12, 12), facecolor = 'k', edgecolor = 'k' ) 
+    plt.imshow(wordcloud) 
+    plt.axis("off") 
+    plt.tight_layout(pad = 0) 
+    plt.show()
+
+text = train.text.values
+Plot_world(text)
+print(train.keyword.nunique())
+
+"""train.keyword.value_counts()[:20].iplot(kind='bar', title='Top 20 keywords in text', color='red')
+train.location.value_counts()[:20].iplot(kind='bar', title='Top 20 location in tweet', color='blue')  # Check the top 15 locations 
+"""
+
+
+"""-------"""
+##Bag of Words Counts 
+def cv(data):
+    count_vectorizer = CountVectorizer()
+
+    emb = count_vectorizer.fit_transform(data)
+
+    return emb, count_vectorizer
+
+list_corpus = train["text"].tolist()
+list_labels = train["target"].tolist()
+
+X_train, X_test, y_train, y_test = train_test_split(list_corpus, list_labels, test_size=0.2, random_state=40)
+
+X_train_counts, count_vectorizer = cv(X_train)
+X_test_counts = count_vectorizer.transform(X_test)
+
+
+def plot_LSA(test_data, test_labels, savepath="PCA_demo.csv", plot=True):
+        lsa = TruncatedSVD(n_components=2)
+        lsa.fit(test_data)
+        lsa_scores = lsa.transform(test_data)
+        color_mapper = {label:idx for idx,label in enumerate(set(test_labels))}
+        color_column = [color_mapper[label] for label in test_labels]
+        colors = ['orange','blue']
+        if plot:
+            plt.scatter(lsa_scores[:,0], lsa_scores[:,1], s=8, alpha=.8, c=test_labels, cmap=matplotlib.colors.ListedColormap(colors))
+            orange_patch = mpatches.Patch(color='orange', label='Not')
+            blue_patch = mpatches.Patch(color='blue', label='Real')
+            plt.legend(handles=[orange_patch, blue_patch], prop={'size': 30})
+
+fig = plt.figure(figsize=(16, 16))          
+plot_LSA(X_train_counts, y_train)
+# plt.show()
+
+
+##TF IDF
+def tfidf(data):
+    tfidf_vectorizer = TfidfVectorizer()
+
+    train = tfidf_vectorizer.fit_transform(data)
+
+    return train, tfidf_vectorizer
+
+X_train_tfidf, tfidf_vectorizer = tfidf(X_train)
+X_test_tfidf = tfidf_vectorizer.transform(X_test)
+
+fig = plt.figure(figsize=(16, 16))          
+plot_LSA(X_train_tfidf, y_train)
+plt.show()
+
+
+##GloVe
+def create_corpus_new(df):
+    corpus=[]
+    for tweet in tqdm(df['text']):
+        words=[word.lower() for word in word_tokenize(tweet)]
+        corpus.append(words)
+    return corpus   
+
+corpus=create_corpus_new(train)
+
+
+
+embedding_dict={}
+with open('../input/glove-global-vectors-for-word-representation/glove.6B.100d.txt','r') as f:
+    for line in f:
+        values=line.split()
+        word=values[0]
+        vectors=np.asarray(values[1:],'float32')
+        embedding_dict[word]=vectors
+f.close()
+
+MAX_LEN=50
+tokenizer_obj=Tokenizer()
+tokenizer_obj.fit_on_texts(corpus)
+sequences=tokenizer_obj.texts_to_sequences(corpus)
+
+tweet_pad=pad_sequences(sequences,maxlen=MAX_LEN,truncating='post',padding='post')
+
+word_index=tokenizer_obj.word_index
+print('Number of unique words:',len(word_index))
+
+num_words=len(word_index)+1
+embedding_matrix=np.zeros((num_words,100))
+
+for word,i in tqdm(word_index.items()):
+    if i > num_words:
+        continue
+    
+    emb_vec=embedding_dict.get(word)
+    if emb_vec is not None:
+        embedding_matrix[i]=emb_vec 
+tweet_pad[0][0:]
+
+
+##Baseline Model
+model=Sequential()
+
+embedding=Embedding(num_words,100,embeddings_initializer=Constant(embedding_matrix), input_length=MAX_LEN,trainable=False)
+
+model.add(embedding)
+model.add(SpatialDropout1D(0.2))
+model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+
+optimzer=Adam(learning_rate=2e-3)
+
+model.compile(loss='binary_crossentropy',optimizer=optimzer,metrics=['accuracy'])
+model.summary()
+
+train=tweet_pad[:train.shape[0]]
+test=tweet_pad[train.shape[0]:]
+
+X_train,X_test,y_train,y_test=train_test_split(train,train['target'].values,test_size=0.15)
+print('Shape of train',X_train.shape)
+print("Shape of Validation ",X_test.shape)
+history=model.fit(X_train,y_train,batch_size=2,epochs=10,validation_data=(X_test,y_test),verbose=2)
+
+fig = plt.figure(figsize=(16, 16))          
+plot_LSA(train,train['target'])
+plt.show()
+
+##Showing Confusion Matrix
+predictions = model.predict_classes(X_test) 
+
+# Showing Confusion Matrix
+def plot_cm(y_true, y_pred, title, figsize=(5,4)):
+    cm = confusion_matrix(y_true, y_pred, labels=np.unique(y_true))
+    cm_sum = np.sum(cm, axis=1, keepdims=True)
+    cm_perc = cm / cm_sum.astype(float) * 100
+    annot = np.empty_like(cm).astype(str)
+    nrows, ncols = cm.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            c = cm[i, j]
+            p = cm_perc[i, j]
+            if i == j:
+                s = cm_sum[i]
+                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
+            elif c == 0:
+                annot[i, j] = ''
+            else:
+                annot[i, j] = '%.1f%%\n%d' % (p, c)
+    cm = pd.DataFrame(cm, index=np.unique(y_true), columns=np.unique(y_true))
+    cm.index.name = 'Actual'
+    cm.columns.name = 'Predicted'
+    fig, ax = plt.subplots(figsize=figsize)
+    plt.title(title)
+    sns.heatmap(cm, cmap= "YlGnBu", annot=annot, fmt='', ax=ax)
+
+# Showing Confusion Matrix
+plot_cm(y_test, predictions, 'Confution matrix of Tweets', figsize=(7,7))
+
+
+##Submission 
+submission = pd.read_csv('../input/nlp-getting-started/sample_submission.csv')
+
+predictions = model.predict(test)
+pred =  pd.DataFrame(predictions, columns=['preds'])
+pred.plot.hist()
+
+submission['target'] = (predictions > 0.5).astype(int)
+submission.head(10)
+
+submission.to_csv("submission_GloVe.csv", index=False, header=True)
+
+
+
+"""
+
+##The maximum number of words to be used
+vocab_size = 10000
+embedding_dim = 16
+
+# Max number of words in each complaint.
+max_length = 20
+trunc_type='post'
+padding_type='post'
+
+# oov_took its set for words out our word index
+oov_tok = "<XXX>"
+training_size = 6090
+seq_len = 12
+
+
+##Train Test Split
+training_sentences = train.text[0:training_size]
+training_labels = train.target[0:training_size]
+
+testing_sentences = train.text[training_size:]
+testing_labels = train.target[training_size:]
+
+print('The Shape of training :',training_sentences.shape)
+print('The Shape of testing:',testing_sentences.shape)
+
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+# Lets see the first 10 elements
+print("THe first word Index are: ")
+for x in list(word_index)[0:15]:
+    print (" {},  {} ".format(x,  word_index[x]))
+
+
+
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+print(train.text[1])
+print(training_sequences[1])
+
+
+##check Inverse for see how it works
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+# Lets see the first 10 elements
+print("THe first reverse word Index are: ")
+for x in list(reverse_word_index)[0:15]:
+    print (" {},  {} ".format(x,  reverse_word_index[x]))
+
+
+def decode(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+decode(training_sequences[1]) # this can be usefull for check predictions
+training_padded[1628]
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+
+
+##Creating the Model
+# For a binary classification problem
+model = Sequential()
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Model Definition with LSTM
+model = tf.keras.Sequential([ tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)), tf.keras.layers.Dense(14, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')]) # remember this is a binary clasification
+model.summary()
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+
+from keras.utils.vis_utils import plot_model
+plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+
+start_time = time.time()
+
+num_epochs = 10
+history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels))
+
+final_time = (time.time()- start_time)/60
+print(f'The time in minutos: {final_time}')
+
+model_loss = pd.DataFrame(model.history.history)
+model_loss.head()
+
+model_loss[['accuracy','val_accuracy']].plot(ylim=[0,1])
+plt.show()
+
+# ##Model Evaluation
+predictions = model.predict_classes(testing_padded)   # predict_ clases because is classification problem with the split test
+# predictions
+
+from sklearn.metrics import classification_report,confusion_matrix
+
+# Showing Confusion Matrix
+def plot_cm(y_true, y_pred, title, figsize=(5,4)):
+    cm = confusion_matrix(y_true, y_pred, labels=np.unique(y_true))
+    cm_sum = np.sum(cm, axis=1, keepdims=True)
+    cm_perc = cm / cm_sum.astype(float) * 100
+    annot = np.empty_like(cm).astype(str)
+    nrows, ncols = cm.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            c = cm[i, j]
+            p = cm_perc[i, j]
+            if i == j:
+                s = cm_sum[i]
+                annot[i, j] = '%.1f%%\n%d/%d' % (p, c, s)
+            elif c == 0:
+                annot[i, j] = ''
+            else:
+                annot[i, j] = '%.1f%%\n%d' % (p, c)
+    cm = pd.DataFrame(cm, index=np.unique(y_true), columns=np.unique(y_true))
+    cm.index.name = 'Actual'
+    cm.columns.name = 'Predicted'
+    fig, ax = plt.subplots(figsize=figsize)
+    plt.title(title)
+    sns.heatmap(cm, cmap= "YlGnBu", annot=annot, fmt='', ax=ax)
+
+# Showing Confusion Matrix
+plot_cm(testing_labels,predictions, 'Confution matrix of Tweets', figsize=(7,7))
+plt.show()
+
+
+##Now working with test dataset
+
+testing_sequences2 = tokenizer.texts_to_sequences(test.text)
+testing_padded2 = pad_sequences(testing_sequences2, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+predictions = model.predict(testing_padded2)
+
+pred =  pd.DataFrame(predictions, columns=['preds'])
+pred.plot.hist()
+
+# sample of submission
+submission['target'] = (predictions > 0.5).astype(int)
+submission.head()
+submission.to_csv("submission.csv", index=False, header=True)      """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
